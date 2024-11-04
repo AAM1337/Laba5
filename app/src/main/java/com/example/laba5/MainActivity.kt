aebc9d0f1a88c9fac73f748011aeb4ba4a49bd20
AAM1337 <amacinov@gmail.com>
2024-11-04 14:40:08 +0700

    Создана логика для обеих активностей. Программа полностью рабочая

--- app/src/main/java/com/example/laba5/MainActivity.kt
+++ app/src/main/java/com/example/laba5/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.laba5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btn: Button
+    private lateinit var price: EditText
+    private lateinit var radioButton1: RadioButton
+    private lateinit var radioButton2: RadioButton
+    private lateinit var radioButton3: RadioButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,23 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        radioButton1=findViewById(R.id.radioButton)
+        radioButton2=findViewById(R.id.radioButton2)
+        radioButton3=findViewById(R.id.radioButton3)
+        price=findViewById(R.id.editTextNumberDecimal)
+        btn=findViewById(R.id.button)
+
+        btn.setOnClickListener()
+        {
+            val intent=Intent(this,SecondActivity::class.java)
+            when{
+                radioButton1.isChecked->intent.putExtra("Selected_Radio","RadioButton1")
+                radioButton2.isChecked->intent.putExtra("Selected_Radio","RadioButton2")
+                radioButton3.isChecked->intent.putExtra("Selected_Radio","RadioButton3")
+            }
+            intent.putExtra("Price",price.text.toString())
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/laba5/SecondActivity.kt
+++ app/src/main/java/com/example/laba5/SecondActivity.kt
@@ -1,20 +1,43 @@
 package com.example.laba5
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var text: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_second)
+
+        val Selected_Radio = intent.getStringExtra("Selected_Radio")
+        val priceValue = intent.getStringExtra("Price")?.toDoubleOrNull() ?: 0.0
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        text=findViewById(R.id.textView4)
+        var result:Double=0.0
+        when(Selected_Radio){
+            "RadioButton1"->{
+                result=priceValue*75
+            }
+            "RadioButton2"->{
+                result=priceValue*90
+            }
+            "RadioButton3"->{
+                result=priceValue*100
+            }
+        }
+        text.text=result.toString()
     }
 }
